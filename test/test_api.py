42cfffbca0878704d5c8dc236d08db61cd520a94
Lucas <kbelodiniz@hotmail.com>
2020-05-31 16:56:55 +0100

    Add extra test
    * Test a large list of integers

--- test/test_api.py
+++ test/test_api.py
@@ -1,11 +1,23 @@
 import json
 
 
-def test_total(test_client):
+def test_total_simple(test_client):
 
     response = test_client.post(
         "/total",
         data=json.dumps(dict(numbers=[1, 2, 3])),
-        content_type="application/json")
+        content_type="application/json"
+    )
 
     assert json.loads(response.data) == {"total": 6}
+
+
+def test_total_large(test_client):
+
+    response = test_client.post(
+        "/total",
+        data=json.dumps(dict(numbers=list(range(10000001)))),
+        content_type="application/json"
+    )
+
+    assert json.loads(response.data) == {"total": 50000005000000}
